7d98fb34a4ff9720513cf7e6419e6dbb0a2ec4d8
Kpadhi <coderkali@gmail.com>
2020-11-11 21:24:19 +0530

    image gallery added

--- js/app.js
+++ js/app.js
@@ -55,4 +55,57 @@ $(document).ready(function() {
     sectionObserver.observe(counterSection);
 
 
+    var $wrapper = $('.portfolio_wrapper');
+
+    $wrapper.isotope({
+        filter : '*',
+        layoutMode : 'masonry',
+        animationOptions : {
+           duration : 750,
+           style : 'linear'
+        }
+    });
+
+    let links = document.querySelectorAll('.tabs a');
+
+    links.forEach(link => {
+
+       let selector = link.dataset.filter;
+       link.addEventListener('click', function(e){
+          e.preventDefault();
+
+          $wrapper.isotope({
+            filter : selector,
+            layoutMode : 'masonry',
+            animationOptions : {
+               duration : 750,
+               style : 'linear'
+            }
+        });
+
+        links.forEach(link => {
+         link.classList.remove('active');
+        });
+        e.target.classList.add('active');
+       });
+    });
+
+
+    $('.magnify').magnificPopup({
+       type : 'image',
+       gallery: {
+          enabled : true
+       },
+       zoom: {
+          enable: true
+       }
+    })
+
+
+
+
+
+
+
+
 });
